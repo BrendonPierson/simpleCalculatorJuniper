0dcbfb3feb9aa7f00004f781e07e851d062015d5
BrendonPierson <brendonpierson@gmail.com>
2015-11-19 03:04:47 -0600

    Evaluate class can compute basic operations

---
+++ SimpleCalculator/Evaluate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Evaluate
+    {
+        public int ArgOne { get; set; }
+        public int ArgTwo { get; set; }
+        public string Operation { get; set; }
+
+
+        public Evaluate() { }
+        public Evaluate(int argOne, int argTwo, string operation)
+        {
+            ArgOne = argOne;
+            ArgTwo = argTwo;
+            Operation = operation;
+        }
+
+        public int Compute()
+        {
+            switch (Operation)
+            {
+                case "+":
+                    return Add(ArgOne, ArgTwo);
+                case "-":
+                    return Sub(ArgOne, ArgTwo);
+                case "*":
+                    return Mult(ArgOne, ArgTwo);
+                case "/":
+                    return Div(ArgOne, ArgTwo);
+                case "%":
+                    return Mod(ArgOne, ArgTwo);
+            }
+            return 0;
+        }
+
+        
+
+        public int Add( int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+        public int Mod(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculatorTests/EvaluateTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class EvaluateTests
+    {
+        [TestMethod]
+        public void EvaluateEnsureCanInstantiate()
+        {
+            Evaluate eval = new Evaluate();
+            Assert.IsNotNull(eval);
+        }
+        [TestMethod]
+        public void EvaluateCanAdd()
+        {
+            Evaluate eval = new Evaluate();
+            int actual = eval.Add(5, 2);
+            Assert.AreEqual(7, actual);
+        }
+        [TestMethod]
+        public void EvaluateCanSubtract()
+        {
+            Evaluate eval = new Evaluate();
+            int actual = eval.Sub(5, 2);
+            Assert.AreEqual(3, actual);
+        }
+        [TestMethod]
+        public void EvaluateCanMultiply()
+        {
+            Evaluate eval = new Evaluate();
+            int actual = eval.Mult(5, 2);
+            Assert.AreEqual(10, actual);
+        }
+        [TestMethod]
+        public void EvaluateCanDivide()
+        {
+            Evaluate eval = new Evaluate();
+            int actual = eval.Div(10, 2);
+            Assert.AreEqual(5, actual);
+        }
+        [TestMethod]
+        public void EvaluateCanModulus()
+        {
+            Evaluate eval = new Evaluate();
+            int actual = eval.Mod(10, 2);
+            Assert.AreEqual(0, actual);
+        }
+        [TestMethod]
+        public void EvaluateCanChooseCorrectOperation()
+        {
+            Evaluate eval = new Evaluate(3, 4, "+");
+            var actual = eval.Compute();
+            Assert.AreEqual(7, actual);
+        }
+        [TestMethod]
+        public void EvaluateCanUseParsedString()
+        {
+            Parse parse = new Parse("3 + 9");
+            Evaluate eval = new Evaluate(parse.FirstArg, parse.SecondArg, parse.Operand);
+            var actual = eval.Compute();
+            Assert.AreEqual(12, actual);
+        }
+    }
+}
